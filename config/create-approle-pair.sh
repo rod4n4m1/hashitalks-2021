df8a33603cff8d3e704901660661b09cb24d7dee
Rod Anami <rod.anami@br.ibm.com>
2021-02-16 21:52:00 -0300

    Improved vault configuration script

--- config/create-approle-pair.sh
+++ config/create-approle-pair.sh
@@ -1,5 +1,11 @@
 vault login $VAULT_ROOT_TOKEN
 
+# Enable AppRole auth method for the app
+vault auth enable approle
+
+# Enable KV v2 secret engine for the app
+vault secrets enable kv-v2
+
 vault policy write my-policy my-policy.hcl
 
 vault write auth/approle/role/my-role secret_id_ttl="720h"  token_ttl="12h"  token_max_tll="12h"  policies="my-policy"
